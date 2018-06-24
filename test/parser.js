bf31d4016b771f6fbfc180e89eaabbbe01c54d8d
Giannis Leontiadis <giannisleondiadis@gmail.com>
2018-06-24 23:46:45 +0300

    Add tests for parser.js

--- test/parser.js
+++ test/parser.js
@@ -65,4 +65,19 @@ describe('parse()', function () {
       new ASTOperator('/', new ASTOperator('-', new ASTConstant('20'), new ASTVariable('x')), new ASTVariable('k'))
     )
   })
+  it('should return the corresponding root of the AST for any valid tokenized array (test 5)', function () {
+    expect(parse([
+      new Lexeme('leftParenthesis', '('),
+      new Lexeme('constant', '20'),
+      new Lexeme('operator', '*'),
+      new Lexeme('leftParenthesis', '('),
+      new Lexeme('constant', '10'),
+      new Lexeme('operator', '+'),
+      new Lexeme('variable', 'k'),
+      new Lexeme('rightParenthesis', ')'),
+      new Lexeme('rightParenthesis', ')')
+    ])).to.deep.equal(
+      new ASTOperator('*', new ASTConstant('20'), new ASTOperator('+', new ASTConstant('10'), new ASTVariable('k')))
+    )
+  })
 })
